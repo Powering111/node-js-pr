e5b63c7a4bb77c2edd1b345be374045527fef159
Juntae Jeong <juntaejeonghappy@naver.com>
2021-08-10 18:05:30 +0900

    html, resource request ok illegal path is prevented

---
+++ readme.md
@@ -0,0 +1,11 @@
+# WEB SERVER
+Using Node.JS
+
+## Features
+- make web server
+- use database to login, chat
+- make realtime chatting
+- make some realtime game
+
+## Using modules
+async
--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const http=require('http');
 const fs=require('fs');
 const url=require('url');
 const path=require('path');
-const rootDirectory = __dirname + '\\Files';
-const htmlDirectory = rootDirectory + '\\hypertext\\';
-const resourceDirectory = rootDirectory + '\\resources\\';
+const async=require('async');
+
+const rootDirectory = __dirname + '\\Files\\';
+const htmlDirectory = rootDirectory + 'hypertext\\';
+const resourceDirectory = rootDirectory + 'resources\\';
+const baseDirectory = rootDirectory + 'base\\';
+
 
 const hypertextExt = ['','.','.html'];
 const resourceExt = ['.jpg','.jpeg','.png','.gif','.bmp','.mp3','.mp4'];
+
 
 function respond(req,res){
     
@@ -15,22 +20,85 @@ function respond(req,res){
     let filePath=parsedURL.pathname;
     if(filePath=='/') filePath='/index';
 
-    
-    
     let parsedPath=path.parse(filePath);
     console.log(parsedPath);
-
+    
     if(hypertextExt.includes(parsedPath.ext)){
-        let joinedPath=path.join(htmlDirectory,filePath);
+        let joinedPath=path.join(htmlDirectory,parsedPath.name+'.html');
         if(joinedPath.indexOf(htmlDirectory)!==0){
             console.log('invalid request!');
             return;
         }
-        console.log(joinedPath);
-    }
-    if(resourceExt.includes(parsedPath.ext)){
 
+        fs.readFile(joinedPath,"utf8",(err,data)=>{
+            if(err)send404(res);
+            else sendData(res,data);
+        });
     }
+
+    if(resourceExt.includes(parsedPath.ext)){
+        console.log("resource");
+        let joinedPath=path.join(resourceDirectory,parsedPath.base);
+        if(joinedPath.indexOf(resourceDirectory)!==0){
+            console.log('invalid request!');
+            return;
+        }
+
+        fs.readFile(joinedPath,(err,data)=>{
+            if(err)send404(res);
+            else send200(res,data);
+            
+        });
+    }
+
+}
+
+function replaceAll(str,from,to){
+    return str.split(from).join(to);
+}
+
+function readAsync(fileName, callback) {
+    console.log("reading "+fileName);
+    fs.readFile(fileName, 'utf8', callback);
+}
+function readBase(name,callback){
+    readAsync(baseDirectory+name+".html",callback);
+}
+let baseFiles=['menu','image'];
+function sendData(res,data){
+    async.map(baseFiles, readBase, function(err, basedatas) {
+        if(err){
+            console.log("error");
+            return;
+        }
+        console.log(basedatas);
+        for(i in baseFiles){
+            console.log("${"+baseFiles[i]+"}");
+            data=replaceAll(data,"${"+baseFiles[i]+"}",basedatas[i]);
+        }
+        console.log(data);
+        send200(res,data);
+    });
+    
+}
+
+function send200(res,data){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write(data);
+    res.end();
+}
+
+function send404(res){
+    console.log("HTTP 404");
+    fs.readFile('./Files/hypertext/404.html',(err,data)=>{
+        if(err){
+            data='404 Not Found';
+        }
+        res.writeHead(404,{'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    });
+    
 }
 
 http.createServer(respond).listen(80);
